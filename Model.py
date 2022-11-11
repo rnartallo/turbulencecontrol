0f7a495f42df440b785d3e5b7d6a6b6929a611f1
Ramon Nartallo-Kaluarachchi <nartallokalu@maths.ox.ac.uk>
2022-11-11 13:12:42 +0000

    FFT working for turbulence

--- Model.py
+++ Model.py
@@ -6,20 +6,29 @@ def HopfPhaseModel(phi,t,p):
     N,w,G,delta_x,alpha,D,h = p
     dOsc = np.zeros(N)
     #Form matrices for FFT
-    G_dis = np.zeros(N)
-    E_dis = np.zeros(N,dtype=complex)
+    G_dis = np.zeros(2*N-1)
+    E_dis = np.zeros(2*N-1,dtype=complex)
     C_dis = np.zeros(N,dtype=complex)
     R_dis = np.zeros(N)
+    k = np.zeros(N)
     Theta_dis = np.zeros(N)
     time_step=int(t/h)
+    G_temp=np.zeros(N)
     for n in range(0,N):
-        G_dis[n]=G(n*delta_x)
+        #for j in range(0,N):
+            #k[n]+= G((n-j)*delta_x)
+        G_temp[n]=G(n*delta_x)
         E_dis[n]=cmath.exp(phi[n,time_step]*1j)
-    C_dis=ifft(np.multiply(fft(G_dis),fft(E_dis)))
+    G_temp_flipped = np.flip(G_temp)
+    G_temp_flipped = G_temp_flipped[:len(G_temp_flipped)-1]
+    G_dis =np.concatenate((G_temp_flipped,G_temp))
+    temp=ifft(np.multiply(fft(E_dis),fft(G_dis)))
+    C_dis = temp[int(len(temp)/2):]
     R_dis = np.abs(C_dis)
     Theta_dis = np.angle(C_dis)
+    noise=np.random.normal(0,1,N)
     for n in range(0,N):
-        dOsc[n] = w-R_dis[n]*np.sin(phi[n,time_step]-Theta_dis[n]-alpha)+np.sqrt(D)*np.random.normal(0,1)
+        dOsc[n] = w-R_dis[n]*np.sin(phi[n,time_step]-Theta_dis[n]-alpha)+np.sqrt(D)*noise[n]
     return dOsc
 
 def SolvePhaseHopfModel(phi_0,T,N,w,G,num_points,t_0,delta_x,alpha,D):
--- TurbulenceMeasures.py
+++ TurbulenceMeasures.py
@@ -4,20 +4,46 @@ from scipy.fftpack import fft, ifft
 
 def CalculateLocalKuramotoOrderParam(phi,G,delta_x):
     N,T = np.shape(phi)
-    G_dis = np.zeros(N)
+    G_dis = np.zeros(2*N-1)
     k = np.zeros(N)
-    E_dis = np.zeros(N,dtype=complex)
+    E_dis = np.zeros(2*N-1,dtype=complex)
     C_dis = np.zeros(N,dtype=complex)
+    R_dis = np.zeros((N,T))
+    Theta_dis = np.zeros((N,T))
+    G_temp=np.zeros(N)
+    for n in range(0,N):
+        G_temp[n] = G(n*delta_x)
+        for j in range(0,N):
+            k[n]+= G((n-j)*delta_x)
+    G_temp_flipped = np.flip(G_temp)
+    G_temp_flipped = G_temp_flipped[:len(G_temp_flipped)-1]
+    G_dis =np.concatenate((G_temp_flipped,G_temp))
+    for t in range(0,T):
+        for n in range(0,N):
+            E_dis[n]=cmath.exp(phi[n,t]*1j)
+        temp=ifft(np.multiply(fft(E_dis),fft(G_dis)))
+        C_dis = temp[int(len(temp)/2):]
+        R_dis[:,t] = np.abs(np.multiply(1/k,C_dis))
+        Theta_dis[:,t] = np.angle(np.multiply(1/k,C_dis))
+    return [R_dis,Theta_dis]
+
+def CalculateLocalKOnoFFT(phi,G,delta_x):
+    N,T = np.shape(phi)
     R_dis = np.zeros(np.shape(phi))
-    Theta_dis = np.zeros(np.shape(phi))
+    k = np.zeros(N)
     for n in range(0,N):
         for j in range(0,N):
             k[n]+= G((n-j)*delta_x)
+    Theta_dis = np.zeros(np.shape(phi))
+    C_dis=np.zeros(N,dtype=complex)
+    G_dis = np.zeros(2*N)
+    for n in range(0,N):
+        G_dis[n] = G(n*delta_x)
+        G_dis[-n] = G_dis[n]
     for t in range(0,T):
-        for n in range(0,N):
-            G_dis[n]=G(n*delta_x)
-            E_dis[n]=cmath.exp(phi[n,t]*1j)
-        C_dis=np.multiply(1/k,ifft(np.multiply(fft(G_dis),fft(E_dis))))
-        R_dis[:,t] = np.abs(C_dis)
-        Theta_dis[:,t] = np.angle(C_dis)
+        for i in range(0,N):
+            for j in range(0,N):
+                C_dis[i]+=G_dis[i-j]*cmath.exp(phi[j,t]*1j)
+        R_dis[:,t] = np.abs(np.multiply(1/k,C_dis))
+        Theta_dis[:,t] = np.angle(np.multiply(1/k,C_dis))
     return [R_dis,Theta_dis]
