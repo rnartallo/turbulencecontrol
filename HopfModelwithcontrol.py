14e645bf3bc16416fe70259f9f41c06525db9968
Ramon Nartallo-Kaluarachchi <nartallokalu@maths.ox.ac.uk>
2022-11-14 13:50:51 +0000

    Hopf model with control

--- HopfModelwithcontrol.py
+++ HopfModelwithcontrol.py
@@ -2,8 +2,8 @@ import numpy as np
 import cmath
 from scipy.fftpack import fft, ifft
 
-def ComplexHopfModel(W,t,p):
-    N,G,delta_x,alpha,K,h,beta,w = p
+def ComplexHopfModelWithControl(W,t,p):
+    N,G,delta_x,alpha,K,h,beta,w,mu = p
     dW = np.zeros(N,dtype=complex)
     #Form matrices for FFT
     G_dis = np.zeros(2*N-1)
@@ -23,18 +23,27 @@ def ComplexHopfModel(W,t,p):
     S_w=ifft(np.multiply(fft(W_padded),fft(G_dis)))
     S_w = S_w[int(len(S_w)/2):]
     k = temp_k[int(len(temp_k)/2):]
+    phi= np.angle(W[:,time_step]) - beta*np.log(np.abs(W[:,time_step]))
+    KOP = CalculateGlobalKuramotoOrderParam(phi)
     for n in range(0,N):
-        dW[n] = (1+w*1j)*W[n,time_step]-(1+beta*1j)*(np.abs(W[n,time_step])**2)*W[n,time_step]+K*(1/k[n])*S_w[n]
+        dW[n] = (1+w*1j)*W[n,time_step]-(1+beta*1j)*(np.abs(W[n,time_step])**2)*W[n,time_step]+K*(1/k[n])*S_w[n] + mu*(KOP - W[n,time_step])
     return dW
 
-def SolveComplexHopfModel(W_0,T,N,w,G,num_points,t_0,delta_x,alpha,sigma,K,beta):
+def SolveComplexHopfModelWithControl(W_0,T,N,w,G,num_points,t_0,delta_x,alpha,sigma,K,beta,mu):
     t = np.linspace(t_0,T,num_points)
     W = np.zeros((N,num_points),dtype=complex)
     W[:,0]=W_0
     h=(T-t_0)/num_points
-    p=[N,G,delta_x,alpha,K,h,beta,w]
+    p=[N,G,delta_x,alpha,K,h,beta,w,mu]
     noise=np.random.normal(0,np.sqrt(h),(N,num_points))
     wiener = np.cumsum(noise,axis=1)
     for n in range(0,num_points-1):
-        W[:,n+1] = W[:,n]+ h*ComplexHopfModel(W,t[n],p) + np.sqrt(sigma)*h*(wiener[:,n+1]-wiener[:,n])
+        W[:,n+1] = W[:,n]+ h*ComplexHopfModelWithControl(W,t[n],p) + np.sqrt(sigma)*h*(wiener[:,n+1]-wiener[:,n])
     return [W,t]
+
+def CalculateGlobalKuramotoOrderParam(phi):
+    N = np.shape(phi)[0]
+    Z=0
+    for n in range(0,N):
+        Z+=(1/N)*cmath.exp(phi[n]*1j)
+    return Z
