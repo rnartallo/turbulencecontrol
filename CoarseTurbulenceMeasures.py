0998853d3d9c663ff2cdcc006b8c6c3cacb72be3
Ramon Nartallo-Kaluarachchi <nartallokalu@maths.ox.ac.uk>
2022-11-14 11:33:21 +0000

    Coarse turbulence measures

--- CoarseTurbulenceMeasures.py
+++ CoarseTurbulenceMeasures.py
@@ -11,16 +11,16 @@ def CalculateEdgeCenteredMatrix(phi):
     for n in range(0,int(N*(N-1)/2)):
         i,j = pairs[n]
         for t in range(0,T):
-            E[n,t]=np.sqrt((np.cos(phi[i,t])-np.cos(phi[j,t]))^2 + (np.sin(phi[i,t])-np.sin(phi[j,t]))^2)
+            E[n,t]=np.sqrt((np.cos(phi[i,t])-np.cos(phi[j,t]))**2 + (np.sin(phi[i,t])-np.sin(phi[j,t]))**2)
     return [E,pairs]
 
-def CalculateEdgeMetastability(E):
+def CalculateVariance(E):
     return np.var(E)
 
 def Demean(E):
     M,T = np.shape(E)
     E_means = np.mean(E,axis=0)
-    E_bar = np.zeros(M,T)
+    E_bar = np.zeros((M,T))
     for m in range(0,M):
         E_bar[m,:]=E[m,:]-E_means[m]
     return(E_bar)
@@ -31,11 +31,12 @@ def CalculateEdgeSpaceTimeProb(E,tau_range):
     C = np.zeros((M,len(tau_range)))
     E_bar = Demean(E)
     for m in range(0,M):
-        for tau in tau_range:
+        for i in range(0,len(tau_range)):
+            tau = tau_range[i]
             num=0;den1 =0;den2 =0
             for t in range(tau,T):
                 num+=E_bar[m,t-tau]*E_bar[m,t]
-                den1+=E_bar[m,t-tau]^2
-                den2+=E_bar[m,t]^2
-            C[m,tau]=(num)/(np.sqrt(den1)*np.sqrt(den2))
+                den1+=E_bar[m,t-tau]**2
+                den2+=E_bar[m,t]**2
+            C[m,i]=(num)/(np.sqrt(den1)*np.sqrt(den2))
     return C
