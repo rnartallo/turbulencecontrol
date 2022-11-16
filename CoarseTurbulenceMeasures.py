Fixing variance calculation

CoarseTurbulenceMeasures.py
@@ -14,8 +14,15 @@ def CalculateEdgeCenteredMatrix(phi):
             E[n,t]=np.sqrt((np.cos(phi[i,t])-np.cos(phi[j,t]))**2 + (np.sin(phi[i,t])-np.sin(phi[j,t]))**2)
     return [E,pairs]
 
-def CalculateVariance(E):
-    return np.var(E)
+def CalculateVariance(R):
+    N,T = np.shape(R)
+    R_squaredsum = 0
+    R_sum=0
+    for n in range(0,N):
+        for t in range(0,T):
+            R_squaredsum+=R[n,t]**2
+            R_sum+=R[n,t]
+    return (1/(N*T))*R_squaredsum-((1/(N*T))*R_sum)**2
 
 def Demean(E):
     M,T = np.shape(E)

TurbulenceMeasures.py
@@ -63,4 +63,29 @@ def CalculateGlobalKuramotoOrderParam(phi):
     return [R,Theta]
 
 def CalculateVarianceLKOP(R):
-    return np.var(R)
+    N,T = np.shape(R)
+    R_squaredsum = 0
+    R_sum=0
+    for n in range(0,N):
+        for t in range(0,T):
+            R_squaredsum+=R[n,t]**2
+            R_sum+=R[n,t]
+    return (1/(N*T))*R_squaredsum-((1/(N*T))*R_sum)**2
+
+def CalculateLKOPNetwork(phi,A):
+    N,T = np.shape(phi)
+    Z = np.zeros(N,dtype = complex)
+    R = np.zeros(np.shape(phi))
+    Theta = np.zeros(np.shape(phi))
+    K = np.zeros(N)
+    for n in range(0,N):
+        for j in range(0,N):
+            K[n]+=A[n,j]
+    for t in range(0,T):
+        Z = np.zeros(N,dtype = complex)
+        for n in range(0,N):
+            for j in range(0,N):
+                Z[n] += A[n,j]*cmath.exp(phi[j,t]*1j)
+        R[:,t] = np.abs(np.multiply(1/K,Z))
+        Theta[:,t] = np.angle(np.multiply(1/K,Z))
+    return [R,Theta]